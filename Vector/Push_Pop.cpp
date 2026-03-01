ac5fab4a2948c0bb4e0abc207fed390a7c2513f2
NDMinhThang <glucozoww@gmail.com>
2026-03-01 22:04:52 +0700

    Add push and pop operations for vector
    Implement push and pop functionality for a vector based on user input.

--- Vector/Push_Pop.cpp
+++ Vector/Push_Pop.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using ll = long long;
+using namespace std;
+	int main(){
+		int n; cin>>n;
+		vector<int> v;
+		for(int i = 0; i<n; i++){
+			int m;
+			cin>>m;
+			if(m==1){
+				int a;
+				cin>>a;
+				v.push_back(a);
+			} 
+			if(m==2 && !v.empty()) v.pop_back();		
+		}
+		if(v.empty()) cout<<"EMTRY"<<endl;
+		else
+			for(int x:v){
+			cout<<x<<" ";
+		}
 
+	}
