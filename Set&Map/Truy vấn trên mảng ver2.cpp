3eeddc7329369f362fa3ce60c2dd1433a55477db
NDMinhThang <glucozoww@gmail.com>
2026-03-26 03:31:20 +0700

    Implement map queries for array elements

--- Set&Map/Truy vấn trên mảng ver2.cpp
+++ Set&Map/Truy vấn trên mảng ver2.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+  int main(){
+      map<int,int> mp;
+      int n;
+      cin >> n;
+      for(int i = 0; i < n; i++){
+              int m;
+              cin>>m;
+              mp[m]++;
+      }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int type, x;
+        cin>>type>>x;
+        if(type == 1){
+            mp[x]++;
+        }
+        else if(type == 2){
+            if(mp[x]>0) mp[x]--;
+        }
+        else{
+            if(mp[x] > 0) cout<<"YES"<<endl;
+            else cout<<"NO"<<endl;
+        }
+
+    }
+
+}
 
