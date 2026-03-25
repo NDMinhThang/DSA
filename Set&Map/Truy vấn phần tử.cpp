4f9a6cb3576a7c19a0254d1406c4a046fa763bc8
NDMinhThang <glucozoww@gmail.com>
2026-03-26 02:11:00 +0700

    Update Truy vấn phần tử.cpp

--- Set&Map/Truy vấn phần tử.cpp
+++ Set&Map/Truy vấn phần tử.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+    int main(){
+        int n; cin>>n;
+        set<int> se;
+        for(int i = 0; i<n; i++){
+            int x;
+            cin>>x;
+            se.insert(x);
+        }
+        
+        int q;
+        cin>>q;
+            while(q--){
+                int x;
+                cin>>x;
+                if(se.find(x) != se.end())
+                    cout<<"YES"<<endl;
+                else
+                    cout<<"NO"<<endl;
+
+            }
+    }
