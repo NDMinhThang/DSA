4cd47159c6a8c40951f9c2efb69a080bd177349e
NDMinhThang <glucozoww@gmail.com>
2026-04-13 19:16:19 +0700

    Implement graph conversion from adjacency list to edge list

--- Đồ thị/Danh sách kề sang danh sách cạnh (vô hướng).cpp
+++ Đồ thị/Danh sách kề sang danh sách cạnh (vô hướng).cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    int main(){
+        int n;
+        cin>>n;
+        cin.ignore();
+
+        for(int i = 1; i<=n; i++){
+            string s, num;
+            getline(cin, s);
+            stringstream ss(s);
+
+            while(ss >> num){
+                if(stoi(num)>i){
+                    cout<<i<<" "<<num<<endl; // stoi chuyển chuỗi thành số
+                }
+            }
+        }
+    }
 
