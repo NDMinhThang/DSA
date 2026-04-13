c6a58ed8d62b0604af4e270f1d3a0e4bb81ce335
NDMinhThang <glucozoww@gmail.com>
2026-04-13 19:11:39 +0700

    Update Danh sách cạnh sang danh sách kề (vô hướng).cpp

--- Đồ thị/Danh sách cạnh sang danh sách kề (vô hướng).cpp
+++ Đồ thị/Danh sách cạnh sang danh sách kề (vô hướng).cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[1001];
+
+    int main(){ 
+        int n,m;
+        cin>>n>>m;        
+
+        for(int i = 0; i<m; i++){
+            int x,y;
+            cin>>x>>y;
+            
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+      
+        for(int i = 1; i<=n; i++){
+            sort(adj[i].begin(), adj[i].end());
+
+            cout<<i<<" : ";
+            
+            for(int x:adj[i]){
+                cout<<x<<' ';
+            }
+            cout<<endl;
+        }
+        
+    }
 
