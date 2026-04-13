23e9d9494bbaaa37efbdb3e4f51f7b48d0912a32
NDMinhThang <glucozoww@gmail.com>
2026-04-13 19:18:05 +0700

    Update Ma trận kề sang danh sách cạnh (vô hướng).cpp

--- Đồ thị/Ma trận kề sang danh sách cạnh (vô hướng).cpp
+++ Đồ thị/Ma trận kề sang danh sách cạnh (vô hướng).cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// từ ma trận kề chuyển sang danh sách cạnh
+int a[1001][1001];
+vector<pair<int,int>> edge;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // nhập ma trận kề
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    // chuyển sang danh sách cạnh
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(a[i][j] == 1){
+                edge.push_back({i, j});
+            }
+        }
+    }
+
+    // in danh sách cạnh
+    for(auto it : edge){
+        cout << it.first << " " << it.second << endl;
+    }
+}
