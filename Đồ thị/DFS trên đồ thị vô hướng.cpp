d9c0cc475f4034d49f79f4d9d89c2a79b77ccdba
NDMinhThang <glucozoww@gmail.com>
2026-04-13 19:24:41 +0700

    Add DFS implementation for undirected graph
    Implement Depth First Search (DFS) for an undirected graph.

--- Đồ thị/DFS trên đồ thị vô hướng.cpp
+++ Đồ thị/DFS trên đồ thị vô hướng.cpp
@@ -1,1 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+   int n, m, s;
+   vector<int> adj[1001];
+   bool visited[1001];
+
+   void inp() {
+    cin>>n>>m>>s;
+    for(int i = 0; i<m; i++){
+        int x, y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+
+    for(int i = 1; i <= n; i++){
+        sort(adj[i].begin(), adj[i].end());
+    }
+
+        memset(visited, false, sizeof(visited)); 
+   }
+
+
+   void dfs(int u){
+
+    cout<< u << " ";
+    // danh dau la u da duoc tham 
+    visited[u] = true;
+    for(int v : adj[u]){
+        // neu dinh v chua duoc tham
+        if(!visited[v]){
+            dfs(v);
+        }
+    }
+
+   }
+
+   int main(){
+        inp();
+        dfs(s);
+   }
 
