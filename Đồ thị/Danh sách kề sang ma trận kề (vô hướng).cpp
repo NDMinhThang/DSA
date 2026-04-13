d9f9532bcfdb4bf39403a0cb970acf384633b117
NDMinhThang <glucozoww@gmail.com>
2026-04-13 19:21:41 +0700

    Implement adjacency matrix from adjacency list

--- Đồ thị/Danh sách kề sang ma trận kề (vô hướng).cpp
+++ Đồ thị/Danh sách kề sang ma trận kề (vô hướng).cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+    vector<pair<int, int>> edge;
+    int a[1001][1001];
+
+    int main(){
+        int n; cin>>n;
+        cin.ignore();
+
+        for(int i = 1; i<=n; i++){
+            string s; int v;
+            getline(cin,s);
+            stringstream ss(s);
+
+            while(ss >> v){
+                a[i][v] = 1;             
+            }
+
+        }
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                cout<<a[i][j]<<' ';
+            }
+            cout<<endl;
+        }
+
+    }
