644295ccb4ae793b745814c6d1e7080a9730b579
NDMinhThang <glucozoww@gmail.com>
2026-04-23 00:20:10 +0700

    Update Phần tử bên phải đầu tiên lớn hơn.cpp

--- Stack & Queue/Phần tử bên phải đầu tiên lớn hơn.cpp
+++ Stack & Queue/Phần tử bên phải đầu tiên lớn hơn.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> a(n), res(n);
+        for(int &x : a) cin >> x;
+
+        stack<int> st;
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= a[i]){
+                st.pop();
+            }
+
+            if(st.empty()) res[i] = -1;
+            else res[i] = st.top();
+
+            st.push(a[i]);
+        }
+
+        for(int x : res) cout << x << " ";
+        cout << endl;
+    }
+}
