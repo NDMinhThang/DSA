199ed867da8cf3f6e6073fa46dd095be4c2377f8
NDMinhThang <glucozoww@gmail.com>
2026-04-23 00:15:40 +0700

    Update Ngăn xếp 2.cpp

--- Stack & Queue/Ngăn xếp 2.cpp
+++ Stack & Queue/Ngăn xếp 2.cpp
@@ -1,1 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+	int main(){
+      int t; cin>>t;
+      cin.ignore();
+			stack<int> st;
+			while(t--){
+        string s;
+				getline(cin,s);
+				stringstream ss(s);
+				string check;
+
+				ss >> check;
+
+				if(check == "PUSH"){
+					int x;
+					ss >> x;
+					st.push(x);
+				}
+
+				else if(check == "POP"){
+					if(!st.empty()) st.pop();
+
+				}
+
+				else if(check == "PRINT"){
+					if(st.empty()){
+						cout<<"NONE"<<endl;
+					}
+					else{
+
+						if(!st.empty()){
+							cout<<st.top();
+						}
+						cout<<endl;
+
+					}
+
+				}
+
+			}			
+	}
