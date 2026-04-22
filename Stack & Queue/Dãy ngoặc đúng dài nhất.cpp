3f5c74e9a25b88a0db4ef7db0744311ec73aa56b
NDMinhThang <glucozoww@gmail.com>
2026-04-23 00:17:55 +0700

    Update Dãy ngoặc đúng dài nhất.cpp

--- Stack & Queue/Dãy ngoặc đúng dài nhất.cpp
+++ Stack & Queue/Dãy ngoặc đúng dài nhất.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+   void solve(){
+        string s; cin>>s;
+        
+
+        stack<int> st;
+        st.push(-1); // mốc ban đầu = -1
+        int maxx = 0;
+
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else{
+
+                st.pop();
+                if(!st.empty()) maxx = max(maxx, i - st.top()); // độ dài bằng kết thúc trừ index trước bắt đầu
+                else st.push(i);
+
+            } 
+
+        }
+        cout<<maxx<<endl;
+    }
+
+    int main(){
+        int t; cin>>t;
+        while(t--){
+            solve();
+        }
+        
+    }
