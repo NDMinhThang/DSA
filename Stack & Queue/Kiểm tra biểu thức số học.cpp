bb619647eac4017833d82eb2067fd6f867864e2f
NDMinhThang <glucozoww@gmail.com>
2026-04-23 17:19:57 +0700

    Implement arithmetic expression validation
    Add logic to check for valid arithmetic expressions using a stack.

--- Stack & Queue/Kiểm tra biểu thức số học.cpp
+++ Stack & Queue/Kiểm tra biểu thức số học.cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+   void solve(){
+        string s;
+        getline(cin, s);
+        stack<char> st;
+        bool check;
+        for(int i = 0; i<s.size(); i++){
+            
+            if(s[i] == ')'){
+
+            check = true;
+            while(!st.empty() && st.top() != '('){
+                if(st.top() == '+' || st.top() == '-' || st.top() == '*' || st.top() == '/'){
+                    check = false;
+                }
+                st.pop();
+            }
+
+            if(!st.empty()) st.pop();
+
+            if(check) break;
+
+            }
+            else st.push(s[i]);
+
+            } 
+
+            if(check) cout<<"Yes"<<endl;
+            else cout<<"No"<<endl;
+        }   
+
+
+    int main(){
+        int t; cin>>t;
+        cin.ignore();
+        while(t--){
+            solve();
+        }
+        
+    }
