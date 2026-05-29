1e3e9258354c4012c54abbe1b3b87975c207032c
NDMinhThang <glucozoww@gmail.com>
2026-05-29 20:49:48 +0700

    Add next permutation function for string input
    Implement next permutation function for a string.

--- Thuật toán sinh/Hoán vị tiếp theo của chuỗi số.cpp
+++ Thuật toán sinh/Hoán vị tiếp theo của chuỗi số.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+string Sinh(string s) {
+    // Nếu hàm next_permutation trả về true, xâu s đã tự động được đổi sang cấu hình kế tiếp
+    if (next_permutation(s.begin(), s.end())) {
+        return s;
+    }
+    // Nếu không thể sinh thêm (xâu đã giảm dần hoàn toàn), trả về "BIGGEST"
+    return "BIGGEST";
+}
+
+int main() {
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            int a;
+            string s;
+            cin >> a >> s;
+            cout << a << " " << Sinh(s) << endl;
+        }
+    }
+}
