65f8c8705c18cabeb306337bab50e74e68434147
NDMinhThang <glucozoww@gmail.com>
2026-05-31 16:09:43 +0700

    Implement binary tree leaf node counting

--- Cây nhị phân/Đếm node lá.cpp
+++ Cây nhị phân/Đếm node lá.cpp
@@ -1,1 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+    
+    struct node{
+        int data;
+        node *left;
+        node *right;
+
+        node(int x){
+            data = x;
+            left = right = NULL;
+        }
+
+    };
+
+    void insertNode(node* root, int u, int v, char c){
+        if(root == NULL) return;
+
+        if(root->data == u){
+            if(c == 'L') root->left = new node(v);
+            else root->right = new node(v);
+        }
+        else{
+            insertNode(root->left, u, v, c);
+            insertNode(root->right, u, v, c);
+        }
+
+    }
+
+    int count(node* root) {
+        if (root == NULL) return 0;
+        if (root->left == NULL && root->right == NULL) {
+            return 1; 
+        }
+        return count(root->left) + count(root->right);
+    }
+
+    int main(){
+        int n; cin>>n;
+        node *root = NULL;
+
+        while(n--){
+            int u, v; char c;
+            cin>>u>>v>>c;
+
+            if(root == NULL){
+                root = new node(u);
+                insertNode(root, u, v, c);
+            }
+
+            else{
+                insertNode(root, u, v, c);
+            }
+        }
+
+        cout<<count(root)<<endl;
+    }
