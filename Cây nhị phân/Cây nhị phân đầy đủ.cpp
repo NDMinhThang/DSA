e54a2b6ddd9ed47ed19e90c5988b91c61c63230a
NDMinhThang <glucozoww@gmail.com>
2026-05-31 17:16:04 +0700

    Add binary tree insertion and validation functions
    Implement binary tree insertion and check for full tree.

--- Cây nhị phân/Cây nhị phân đầy đủ.cpp
+++ Cây nhị phân/Cây nhị phân đầy đủ.cpp
@@ -1,1 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+    struct node{
+        int data;
+        node *left;
+        node *right;
+
+        node(int x){
+            data = x;
+            left = right = NULL;
+        }
+
+    };
+
+    // Hàm chèn node con vào đúng vị trí của node cha trên cây
+    void insertNode(node* root, int u, int v, char c) {
+        if (root == NULL) return;
+        
+        if (root->data == u) {
+            if (c == 'L') root->left = new node(v);
+            else root->right = new node(v);
+        } else {
+            insertNode(root->left, u, v, c);
+            insertNode(root->right, u, v, c);
+        }
+    }
+
+bool check(node* root) {
+    if (root == NULL) return true;
+
+    if (root->left == NULL && root->right == NULL) return true;
+
+    if (root->left != NULL && root->right != NULL) {
+        return check(root->left) && check(root->right);
+    }
+    return false;
+}
+
+    int main(){
+        int n; cin>>n;
+        node *root = NULL;
+        while(n--){
+        int u, v; char c;
+        cin>>u>>v>>c;
+        
+        if(root == NULL){
+            root = new node(u);
+            insertNode(root, u, v, c);
+        }
+        else{
+            insertNode(root, u, v, c);
+        }
+        }
+        if(check(root)) cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";
+        
+    }
