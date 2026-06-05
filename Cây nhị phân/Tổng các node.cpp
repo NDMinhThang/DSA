9e3413683a7c1409595b480e373af1f6f6a8278f
NDMinhThang <glucozoww@gmail.com>
2026-06-05 21:48:55 +0700

    Add binary tree implementation with sum function
    Implement a binary tree structure with node insertion and sum calculation.

--- Cây nhị phân/Tổng các node.cpp
+++ Cây nhị phân/Tổng các node.cpp
@@ -1,1 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+    struct node{
+        int data;
+
+        node *left;
+        node *right;
+
+        node(int x){
+            data = x;
+            left = right = NULL;
+        }
+
+    };
+
+
+    void insertNode(node* root, int u, int v, char c){
+
+        if(root == NULL) return;
+        if(root->data == u){
+            if(c == 'L') root->left = new node(v);
+            else root->right = new node(v);
+        } else {
+            insertNode(root->left, u, v, c);
+            insertNode(root->right, u, v, c);
+        }
+    }
+
+    int sumAllNodes(node* root) {
+ 
+        if (root == NULL) return 0;
+
+        return root->data + sumAllNodes(root->left) + sumAllNodes(root->right);
+    }
+
+    int main(){
+        int n; cin>>n;
+        node *root = NULL;
+        while(n--){
+            int u, v; char c;
+            cin>>u>>v>>c;
+
+            if(root == NULL){
+                root = new node(u);
+                insertNode(root, u, v, c);
+
+            }
+            else
+                insertNode(root, u, v, c);
+
+        }
+
+        cout<<sumAllNodes(root)<<endl;
+        
+    }
