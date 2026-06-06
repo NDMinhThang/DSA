c7a9583fc12498e10142315659b4ebc669423c0b
NDMinhThang <glucozoww@gmail.com>
2026-06-06 15:58:25 +0700

    Add post-order traversal function for binary tree
    Implement post-order traversal for binary tree using pre-order and in-order arrays.

--- Cây nhị phân/Duyệt cây.cpp
+++ Cây nhị phân/Duyệt cây.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+    
+    int search(int in[], int x, int n){
+        for(int i = 0; i<n; i++){
+            if(in[i] == x) return i; // chi so cua goc cay
+        }
+            return -1;
+    }
+
+    void postOrder(int in[], int pre[], int n){
+        int root = search(in, pre[0], n); // chi so cua goc cay trong mang in order
+      
+        if(root != 0){
+            postOrder(in, pre+1, root);
+        }
+        
+        if(root != n-1){
+            postOrder(in+root+1, pre+root+1, n-root-1);
+        }
+        cout<<pre[0]<<" ";
+    }
+
+    int main(){
+            int n; cin>>n;
+            int in[n], pre[n];
+            for(int i = 0; i<n; i++) cin>>pre[i];
+            for(int i = 0; i<n; i++) cin>>in[i];
+            postOrder(in, pre, n);
+            cout<<endl;
+    }
