b51091ae8c6d0cb5e928f91b09343a855d07cdfb
NDMinhThang <glucozoww@gmail.com>
2026-06-11 14:34:49 +0700

    Implement permutation generation for input numbers

--- Thuật toán sinh/CON1_06 - SINH HOÁN VỊ.cpp
+++ Thuật toán sinh/CON1_06 - SINH HOÁN VỊ.cpp
@@ -1,1 +1,21 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int a[1001], n;
+	int main(){
+        int t; cin>>t;
+        while(t--){
+       		cin>>n;
+            for(int i = 1; i<=n; i++){
+                a[i] = i;
+            }
+            do{
+                for(int i = 1; i<=n; i++){
+                    cout<<a[i];
+                }
+                cout<<" ";
+            }
+            while(next_permutation(a+1, a+n+1));
+            cout<<endl;
+        }       
+       
+    }
